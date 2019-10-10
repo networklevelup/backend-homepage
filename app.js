38d143118a43b2644e7b053fdf5ad084e6266568
unknown <raul@network-levelup.com>
2019-10-10 10:46:37 +0200

    again cors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const createError = require("http-errors");
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors());
+// app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(
