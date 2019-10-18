47c085c9f0eeb05fae022057fe4748f89dfbaf98
unknown <raul@network-levelup.com>
2019-10-18 10:23:45 +0200

    position cors app.js

--- app.js
+++ app.js
@@ -14,20 +14,13 @@ let apiCompanies = require("./routes/apiCompanies");
 let apiLinkedin = require("./routes/apiLinkedin");
 
 const app = express();
-app.use(cors());
-// // Configurar cabeceras y cors
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
+
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(cors());
 
 
 app.use(logger("dev"));
